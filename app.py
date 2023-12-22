80ff50594598323694411b22e138ac155cce241c
Zachary Dj <zachary@zacharys-mbp.home>
2023-12-22 11:44:21 -0500

    added Todo class and initialized database

--- app.py
+++ app.py
@@ -1,6 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    #id of each entry
+    id = db.Column(db.Integer, primary_key=True)
+    #content holds each task
+    #nullable=False to disable creating empty task
+    content = db.Column(db.String(200), nullable=False)
+    #bookeeping
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
 
 @app.route("/")
 def index():
